5244ab2ac92bd1946b96e340c553c07dd708b035
Ravi Kiran K N <ravi.kiran@samsung.com>
2014-12-02 13:21:41 +0530

    mm_file_format_mp3: Remove unnecessary assignments
    Removed assignments which do not serve any purpose
    
    Change-Id: Icf9253abb4fbaf024c9958adbfaf383076b37983
    Signed-off-by: Ravi Kiran K N <ravi.kiran@samsung.com>

--- formats/ffmpeg/mm_file_format_mp3.c
+++ formats/ffmpeg/mm_file_format_mp3.c
@@ -363,7 +363,6 @@ __AvGetXingHeader( AvXHeadData* headData,  unsigned char *buf )
 	if( headFlags & VBR_SCALE_FLAG )
 	{
 		headData->vbrScale = __AvExtractI4( buf );
-		buf+=4;
 	}
 
 	#ifdef __MMFILE_TEST_MODE__
@@ -415,7 +414,6 @@ __AvGetVBRIHeader( AvVBRIHeadData* headData,  unsigned char *buf )
 	headData->sizePerTable = __AvExtractI2( buf );
 	buf+=2;
 	headData->framesPerTable = __AvExtractI2( buf );
-	buf+=2;
 
 	#ifdef __MMFILE_TEST_MODE__
 	debug_msg ("Vbri header: sampling-rate:%d, stream-size:%d, frame-number:%d\n",
